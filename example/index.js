f20aafa96248e472b677d18a6eab235cec66887a
Jesse Shawl <jesse@jshawl.com>
2016-08-08 15:37:04 -0400

    dont modify app view engine

--- example/index.js
+++ example/index.js
@@ -2,11 +2,30 @@ var app = require("express")()
 var bee = require("../index")
 
 app.get("/", function(req, res){
-  console.log(o)
+  res.send([
+    "render engine error",
+    "render template error",
+    "reference error",
+    "type error",
+    "404"
+  ].map(function(e){
+    var href = "/" + e.replace(/\s/g,"-")
+    return "<a href='"+href+"'>"+e+"</a>"
+  }).join("<br>"))
 })
 
-app.get("/render-error", function(req, res){
+app.get("/render-engine-error", function(req, res){
   res.render("fake-view")
+})
+app.get("/render-template-error", function(req, res){
+  app.set("view engine","hbs")
+  res.render("fake-view")
+})
+app.get("/reference-error", function(req, res){
+  res.send(o)
+})
+app.get("/type-error", function(req, res){
+  res.send(null.flooglehorn())
 })
 
 app.use(bee(app))
--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 var fs = require("fs")
+var Handlebars = require("handlebars")
 module.exports = function(app){
-  app.set("view engine","hbs")
-  app.set('views', __dirname)
   app.use(function(req, res, next){
-    res.status(404).render("404",{
+    res.status(404).send(render("404",{
       path: req.path,
       method: req.method.toLowerCase()
-    })
+    }))
   })
   return function(err, req, res, next){
     var inFile = info(err.stack)
     if(fs.existsSync(inFile.file)){
-      fiveHundred(inFile, err, res)
+      fiveHundred(inFile, err, res, app)
     } else {
-      missingView(err, res)
+      missingView(err, res,app)
     }
   }
 }
 
-function fiveHundred(inFile, err, res){
+function fiveHundred(inFile, err, res, app){
   var erroredFile = fs.readFileSync(inFile.file,'utf8')
   .split("\n")
   .map(function(e, i){
@@ -31,24 +30,31 @@ function fiveHundred(inFile, err, res){
     return Math.abs(i+1 - inFile.line ) < 3
   })
   .join("")
-  res.render("error",{
+  res.send(render("error",{
     error: err,
     stack: err.stack,
     file: erroredFile,
     filename: inFile.file,
     line: inFile.line,
     column: inFile.column
-  })
+  }))
 }
 
-function missingView(err, res){
-  console.log(err)
+function missingView(err, res, app){
   var errs = err.stack.split("\n") 
-  res.render('render-error',{ 
-    error: errs[0],
-    filename: err.view.name + err.view.ext,
-    directory: err.view.root
-  })
+  if(errs[0].match(/No default engine/)){
+    return res.send(render('render-engine-missing',{
+      error: errs[0]
+    }))
+  }
+  if(errs[0].match(/Failed to lookup view/)){
+    console.log(app.get("views"))
+    return res.send(render('render-template-missing',{
+      error: errs[0],
+      filename: errs[0].match(/"([^\s]+)"/)[1] + "."+ app.get("view engine"),
+      directory: app.get("views")
+    }))
+  }
 }
 
 function info(stack){
@@ -63,4 +69,13 @@ function info(stack){
   }catch(e){
     return false
   }
+}
+
+function render(template, context){
+  var source = fs.readFileSync(__dirname + "/" + template + ".hbs", "utf8")
+  var tpl = Handlebars.compile(source);
+  var html = fs.readFileSync(__dirname + "/header.hbs", "utf8")
+  html += tpl(context)
+  html += fs.readFileSync(__dirname + "/footer.hbs", "utf8")
+  return html
 }
